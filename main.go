11c4e659801d07d6f58d03b152830cb2a56caf90
Elvin Du <nick@yunio.com>
2015-12-17 16:19:04 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	//	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	router.LoadHTMLFiles("index.html")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"t": ""})
+	})
+	router.GET("/convert", Convert)
+
+	router.Run(":8080")
+}
+
+func Convert(ctx *gin.Context) {
+	tStr := ctx.Query("t")
+	t, err := strconv.ParseInt(tStr, 10, 64)
+	if nil != err {
+		ctx.Error(err)
+	}
+
+	ctx.HTML(200, "index.html", gin.H{"t": I2Time(t)})
+}
+
+func I2Time(v int64) time.Time {
+	return time.Unix(v/int64(time.Millisecond), int64(0))
+}
